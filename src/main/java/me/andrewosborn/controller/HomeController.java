4b7e1690bac0868a804562fbe03120da12986661
akosborn <andrewosborn93@gmail.com>
2018-02-12 12:34:09 -0600

    added gson dependency

--- src/main/java/me/andrewosborn/controller/HomeController.java
+++ src/main/java/me/andrewosborn/controller/HomeController.java
@@ -1,5 +1,7 @@
 package me.andrewosborn.controller;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,13 @@ public class HomeController
     @RequestMapping("/")
     public String home()
     {
+        getTeamsFromURL();
+
         return "Welcome to CBB Team Sheets";
+    }
+
+    private void getTeamsFromURL()
+    {
+
     }
 }
