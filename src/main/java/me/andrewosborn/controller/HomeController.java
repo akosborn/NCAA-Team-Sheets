1b612403688e8eec38c73d89deb5786ff4962ad3
akosborn <andrewosborn93@gmail.com>
2018-02-16 13:07:14 -0600

    changed HomeController to @Controller

--- src/main/java/me/andrewosborn/controller/HomeController.java
+++ src/main/java/me/andrewosborn/controller/HomeController.java
@@ -1,14 +1,14 @@
 package me.andrewosborn.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController
 {
     @RequestMapping("/")
     public String home()
     {
-        return "CBB Team Sheets";
+        return "layout_site";
     }
 }
