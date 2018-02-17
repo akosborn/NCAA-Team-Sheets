2803cbd4f8268f9c2611dee3a39d92c34c3857b6
akosborn <andrewosborn93@gmail.com>
2018-02-17 12:02:25 -0600

    displayed home games on home.html

---
+++ src/main/java/me/andrewosborn/controller/TeamController.java
@@ -0,0 +1,31 @@
+package me.andrewosborn.controller;
+
+import me.andrewosborn.model.Team;
+import me.andrewosborn.persistence.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/team")
+@Controller
+public class TeamController
+{
+    private TeamService teamService;
+
+    @Autowired
+    public TeamController(TeamService teamService)
+    {
+        this.teamService = teamService;
+    }
+
+    @RequestMapping(value = "/{teamName}")
+    public String getTeamPage(Model model, @PathVariable String teamName)
+    {
+        Team team =teamService.getByUrlName(teamName);
+        model.addAttribute(team);
+
+        return "team";
+    }
+}
